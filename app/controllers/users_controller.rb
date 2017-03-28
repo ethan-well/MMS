f0ee21c782f71e7396eeead404789845dc3ca1cc
weiwei1 <weiwei1@meizu.com>
2017-03-28 18:35:26 +0800

    add user register and login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,23 @@
 class UsersController < ApplicationController
   def index
-
   end
 
   def new
-    @user = new User
+    @user = User.new()
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to login_path
+    else
+      flash[:error] = "注册失败"
+      render action: 'new'
+    end
+  end
 
-
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,12 @@ Rails.application.routes.draw do
   get 'password_resets/update'
 
   root 'users#index'
+
   resources :users
+  resources :sessions
+
   get '/example', to: 'them#index'
+
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/logout', to: 'sessions#destroy', as: 'logout'
 end
