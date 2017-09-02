41feb0d8d3d660e8b9d00d3056d74e96277564a7
weiwei <wewin@weiweideMacBook-Pro.local>
2017-09-02 15:40:08 +0800

    解决邀请码验证错误

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -68,7 +68,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def invitation_code_validated?
-    u = User.find_by_invitation_code(params[:invitation_code])
+    u = User.find_by_invitation_code(params[:user][:h_invitation_code])
     unless u.present?
       return redirect_to :back, alert: '邀请码无效'
     end
