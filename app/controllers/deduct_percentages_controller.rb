44bdcf9c025659ae7c4fd51d5fa0416660cca80e
wewin <wewin11235@163.com>
2017-10-11 23:27:51 +0800

    提成错误

--- app/controllers/deduct_percentages_controller.rb
+++ app/controllers/deduct_percentages_controller.rb
@@ -1,5 +1,5 @@
 class DeductPercentagesController < ApplicationController
   def index
-    @deduct_percentages = DeductPercentage.find_by_order_user_id(params[:user_id]) || []
+    @deduct_percentages = DeductPercentage.where(low_user_id: params[:user_id])
   end
 end
